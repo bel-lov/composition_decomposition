feb707622db04ee4c5dc05d888a01479dc474d33
bel-lov <belou.lyuba@yandex.ru>
2023-04-07 22:45:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Widgets from './components/Widgets/Widgets';
+import News from './components/News/News';
+import SearchForm from './components/Search/Search';
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <News />
+      <SearchForm />
+      <Banner img={'https://yandex.ru/images/search?from=tabbar&img_url=http%3A%2F%2Fu.9111s.ru%2Fuploads%2F202206%2F10%2F85c1fa5ceaa05d8bc1b3f093609915d0.jpg&lr=2&p=1&pos=4&rpt=simage&text=%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D0%B8%20%D0%BB%D0%BE%D0%B3%D0%BE'} />
+      <Widgets />
+
     </div>
+
   );
 }
 
